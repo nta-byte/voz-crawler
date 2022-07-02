0d172cd0422eeef004453d1a31f271c386a35f03
Cu Ca Rot <thanhhieu0195@gmail.com>
2022-07-02 08:09:36 +0700

    Update clonedb.sh

--- scripts/clonedb.sh
+++ scripts/clonedb.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-docker cp ./crawler/database/db.sql voz-crawler-vozdb-1:/srv/db.sql
+docker cp ./crawler/database/db-latest.sql voz-crawler-vozdb-1:/srv/db-latest.sql
 docker exec -it voz-crawler-vozdb-1 bash -c 'PGPASSWORD=abcd1234 psql -U postgres -c "drop database postgress;"' 
 docker exec -it voz-crawler-vozdb-1 bash -c 'PGPASSWORD=abcd1234 psql -U postgres -c "create database postgress;"' 
-docker exec -it voz-crawler-vozdb-1 bash -c 'PGPASSWORD=abcd1234 psql -U postgres -d postgress -f /srv/db.sql' 
+docker exec -it voz-crawler-vozdb-1 bash -c 'PGPASSWORD=abcd1234 psql -U postgres -d postgress -f /srv/db-latest.sql' 
