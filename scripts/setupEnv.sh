a3c0009fa99406931a78c97bae6098968f3b87e0
anhnt244 <v.anhnt244@vinpearl.com>
2023-08-06 11:20:55 +0700

    update docker

--- scripts/setupEnv.sh
+++ scripts/setupEnv.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-yum install python3 -y
-yum install python3-pip -y
+apt install python3 -y
+apt install python3-pip -y
 python3 -m venv venv
 source ./venv/bin/activate
 pip install --upgrade pip
