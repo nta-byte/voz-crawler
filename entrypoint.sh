42f440158de3ef09833d0a8ffc1d791f8df5b26b
anhnt244 <v.anhnt244@vinpearl.com>
2023-08-09 17:24:23 +0700

    update docker

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-wait-for-it $PG_HOST:5432 -t 60
-wait-for-it $NGINX_HOST:6379 -t 60
-chmod +x scripts/crawl.sh
-./scripts/crawl.sh
+wait-for-it $PG_HOST:$PG_PORT -t 60
+wait-for-it $NGINX_HOST:$NGINX_PORT -t 60
+chmod +x scripts/crawlnew.sh
+./scripts/crawlnew.sh
