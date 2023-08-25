297a24befa7ac76d2be07d01ce21116cf73ea380
anhnt244 <v.anhnt244@vinpearl.com>
2023-08-25 12:31:57 +0700

    update

--- scripts/clonedb.sh
+++ scripts/clonedb.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 docker cp ./crawler/databases/create-db.sql voz-crawler-vozdb-1:/srv/create-db.sql
 docker exec -it voz-crawler-vozdb-1 bash -c 'PGPASSWORD=${POSTGRES_PASSWORD} psql -U ${POSTGRES_USER} -c "drop database ${POSTGRES_DB};"'
-docker exec -it voz-crawler-vozdb-1 bash -c 'PGPASSWORD=${POSTGRES_PASSWORD} psql -U ${POSTGRES_USER} -c "create database postgress;"'
+docker exec -it voz-crawler-vozdb-1 bash -c 'PGPASSWORD=${POSTGRES_PASSWORD} psql -U ${POSTGRES_USER} -c "create database ${POSTGRES_DB};"'
 docker exec -it voz-crawler-vozdb-1 bash -c 'PGPASSWORD=${POSTGRES_PASSWORD} psql -U ${POSTGRES_USER} -d ${POSTGRES_DB} -f /srv/create-db.sql'
