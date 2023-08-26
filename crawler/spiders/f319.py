5a79fb67b40a6ab0f52bc622168e17d99753d220
hiam.tuananh <knight9fury@gmail.com>
2023-08-26 12:27:28 +0700

    update

--- crawler/spiders/f319.py
+++ crawler/spiders/f319.py
@@ -1,6 +1,8 @@
 from datetime import datetime
 from requests import session
 import scrapy
+from scrapy.spiders import Rule
+from scrapy.linkextractors import LinkExtractor
 from sqlalchemy import func
 from sqlalchemy.dialects.postgresql import insert as pg_insert
 
@@ -24,9 +26,10 @@ class F319StockSpider(scrapy.Spider):
     name = 'f319_stock'
     allowed_domains = ['f319.com']
     start_urls = [
-        'https://f319.com',
+        # 'https://f319.com',
         'https://f319.com/threads/phan-tich-ky-thuat-ta-chi-chia-se-kien-thuc-ko-spam.13691'
     ]
+    rules = [Rule(LinkExtractor(), callback='parse_item', follow=True)]
 
     first_time = True
     session = create_session()
@@ -37,24 +40,24 @@ class F319StockSpider(scrapy.Spider):
         self.count = 0
         self.__start_new_thread()
 
-    def parse(self, response):
-        if self.first_time:
-            self.first_time = False
-            newPage = scrapy.Selector(response).xpath(
-                '//ul[@class="pageNav-main"]/li[last()]/a/@href').get()
-            yield scrapy.Request(response.urljoin(newPage))
-        else:
-            comments = scrapy.Selector(response).xpath(
-                '//article[contains(@class, "js-post")]')
-            for comment in comments[::-1]:
-                yield self.process_item(comment)
-
-            next_page_url = response.xpath(
-                '//a[contains(@class, "pageNav-jump--prev")]/@href').get()
-            if next_page_url is not None and self.__verify_link(next_page_url):
-                yield scrapy.Request(response.urljoin(next_page_url))
-            else:
-                print("DONE")
+    # def parse(self, response):
+    #     if self.first_time:
+    #         self.first_time = False
+    #         newPage = scrapy.Selector(response).xpath(
+    #             '//div[@class="PageNav"]/li[last()]/a/@href').get()
+    #         yield scrapy.Request(response.urljoin(newPage))
+    #     else:
+    #         comments = scrapy.Selector(response).xpath(
+    #             '//li[contains(@class, "message")]')
+    #         for comment in comments[::-1]:
+    #             yield self.process_item(comment)
+    #
+    #         next_page_url = response.xpath(
+    #             '//a[contains(@class, "text")]/@href').get()
+    #         if next_page_url is not None and self.__verify_link(next_page_url):
+    #             yield scrapy.Request(response.urljoin(next_page_url))
+    #         else:
+    #             print("DONE")
 
     def process_item(self, comment):
         item = VozCrawlerItem()
@@ -64,8 +67,8 @@ class F319StockSpider(scrapy.Spider):
         topic = comment.xpath(
             './/div[contains(@class, "messageContent")]/article/blockquote/b/text()').extract()
         item['topic'] = ''.join([line for line in topic if line != "\n"])
-        item['time'] = comment.xpath('.//time/@datetime').get()
-        item['id'] = comment.xpath('.//@data-content').get()
+        item['time'] = comment.xpath('.//a[contains(@class, "datePermalink")]/text()').get()
+        item['id'] = comment.xpath('.//@id').get()
         item['author'] = comment.xpath('.//@data-author').get()
         item['spider_id'] = self.spiderRunner.id
         return item
