845d391b6c4e15d9f63fa820d16fa003c7c0801d
hiam.tuananh <knight9fury@gmail.com>
2023-08-26 21:50:41 +0700

    update

--- services/scrapyd/entrypoint.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-wait-for-it $PG_HOST:$PG_PORT -t 60
-wait-for-it $NGINX_HOST:$NGINX_PORT -t 60
-chmod +x scripts/crawlnew.sh
-chmod +x scripts/clonedb.sh
-./scripts/clonedb.sh
-./scripts/crawlnew.sh
