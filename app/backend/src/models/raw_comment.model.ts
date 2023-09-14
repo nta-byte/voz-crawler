a88805199b5793852e850380f5139004cc48ce18
anhnt244 <v.anhnt244@vinpearl.com>
2023-09-14 12:04:12 +0700

    remove and refactor

--- app/backend/src/models/raw_comment.model.ts
+++
@@ -1,7 +0,0 @@
-import { Model } from "objection";
-
-export class RawComment extends Model {
-  static get tableName() {
-    return "voz_rawcomment";
-  }
-}
--- app/shared/src/constants/index.ts
+++
@@ -1,4 +0,0 @@
-export const API_PATHS = {
-  statisStock: "/statis-stocks",
-  latestStock: "/stocks-latest",
-};
